3e3ce190e44ccafc4879dfc07fbca24551dc7031
Xavier Wevers <xavier.wevers@lucky-kat.com>
2023-03-14 15:28:12 +0100

    added Healthbar for enemies

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float hitPoints;
+    public float maxHitPoints = 5;
+    public HealthBarBehaviour Healthbar;
+
+    void Start()
+    {
+        hitPoints = maxHitPoints;
+        Healthbar.SetHealth(hitPoints, maxHitPoints);
+    }
+
+    public void TakeHit(float damage)
+    {
+        hitPoints -= damage;
+        Healthbar.SetHealth(hitPoints, maxHitPoints);
+
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
